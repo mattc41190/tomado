57e9844e7a75699c010acdffd32ba3ef6d845602
mcale <mcale@HomeAway.com>
2017-10-05 14:09:34 -0500

    added webpack

---
+++ app/scripts/bundle.js
@@ -0,0 +1,125 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+function addMinutes(date, minutes) {
+	return new Date(date.getTime() + minutes * 60000);
+}
+
+function millisToMinutesAndSeconds(millis) {
+	var minutes = Math.floor(millis / 60000);
+	var seconds = ((millis % 60000) / 1000).toFixed(0);
+	return minutes + ":" + (seconds < 10
+		? '0'
+		: '') + seconds;
+}
+
+module.exports = {
+    addMinutes,
+    millisToMinutesAndSeconds
+};
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function(__dirname) {const millisToMinutesAndSeconds = __webpack_require__(0).millisToMinutesAndSeconds
+const addMinutes = __webpack_require__(0).addMinutes
+const timer = document.getElementById('timer');
+const pomoBtn = document.getElementById('pomoBtn');
+const soundName = 'alarm'
+const sound = new Audio(__dirname + '/assets/sounds/' + soundName + '.mp3');
+let interval;
+
+function createTimer(minutes) {
+	let startTime = new Date();
+	let endTime = addMinutes(startTime, minutes);
+	interval = setInterval(() => {
+		if (endTime - new Date() < 0) {
+			clearInterval(interval);
+			interval = null;
+			sound.play();
+			return;
+		}
+		timer.innerHTML = millisToMinutesAndSeconds(endTime - new Date());
+	}, 1000)
+}
+
+pomoBtn.addEventListener('click', (e) => {
+	if (interval) {
+		clearInterval(interval);
+		interval = null;
+	}
+	createTimer(25);
+});
+
+/* WEBPACK VAR INJECTION */}.call(exports, "/"))
+
+/***/ })
+/******/ ]);
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,36 +1,22 @@
-// const millisToMinutesAndSeconds = require('./api.js').millisToMinutesAndSeconds
-// const addMinutes = require('./api.js').addMinutes
-var timer = document.getElementById('timer');
-var pomoBtn = document.getElementById('pomoBtn');
-var soundName = 'alarm'
-var sound = new Audio(__dirname + '/assets/sounds/' + soundName + '.mp3');
-var interval;
-
-function addMinutes(date, minutes) {
-	return new Date(date.getTime() + minutes * 60000);
-}
-
-function millisToMinutesAndSeconds(millis) {
-	var minutes = Math.floor(millis / 60000);
-	var seconds = ((millis % 60000) / 1000).toFixed(0);
-	return minutes + ":" + (seconds < 10
-		? '0'
-		: '') + seconds;
-}
+const millisToMinutesAndSeconds = require('./api.js').millisToMinutesAndSeconds
+const addMinutes = require('./api.js').addMinutes
+const timer = document.getElementById('timer');
+const pomoBtn = document.getElementById('pomoBtn');
+const soundName = 'alarm'
+const sound = new Audio(__dirname + '/assets/sounds/' + soundName + '.mp3');
+let interval;
 
 function createTimer(minutes) {
-	var startTime = new Date();
-	var endTime = addMinutes(startTime, minutes);
+	let startTime = new Date();
+	let endTime = addMinutes(startTime, minutes);
 	interval = setInterval(() => {
 		if (endTime - new Date() < 0) {
 			clearInterval(interval);
 			interval = null;
-			timer.innerHTML = 'DONE'
 			sound.play();
 			return;
 		}
 		timer.innerHTML = millisToMinutesAndSeconds(endTime - new Date());
-
 	}, 1000)
 }
 
